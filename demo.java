0bf48692d2cc6fd0e43baed667fda784295ac09c
Dinesh Mudaliar <edinesh.90@gmail.com>
2019-02-07 00:37:20 +0530

    Revert "This was deleted"
    This reverts commit 90c41ab3ba7054e5990d74cb4a87eb0bce4308a4.

---
+++ demo.java
@@ -0,0 +1,7 @@
+#This is a sample program for helloworld
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
