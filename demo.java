90c41ab3ba7054e5990d74cb4a87eb0bce4308a4
Dinesh Mudaliar <edinesh.90@gmail.com>
2019-02-07 00:11:05 +0530

    This was not deleted

--- demo.java
+++
@@ -1,7 +0,0 @@
-#This is a sample program for helloworld
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
-}
